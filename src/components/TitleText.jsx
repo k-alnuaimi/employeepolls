a1aaf386fcb1bb3f129f135138b9f71b162e6fa7
Khaled L <khaled.laywad@gmail.com>
2024-09-19 19:13:30 +0400

    Create TitleText.jsx

---
+++ src/components/TitleText.jsx
@@ -0,0 +1,5 @@
+import { Text } from "@chakra-ui/react";
+const TitleText = (props) => {
+    return <Text fontSize="4em" textAlign="center" mb="4">{props.children}</Text>
+}
+export default TitleText;
