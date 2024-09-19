198c649b41b8f862e682dd4c888aa54f1b4ed43a
Khaled L <khaled.laywad@gmail.com>
2024-09-19 19:05:59 +0400

    Delete TitleText.jsx

--- src/components/TitleText.jsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "@chakra-ui/react";
-
-const TitleText = (props) => {
-
-    return <Text fontSize="4em" textAlign="center" mb="4">{props.children}</Text>
-
-}
-
-export default TitleText;
